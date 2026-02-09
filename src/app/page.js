852f562dbfb4a18c86c24d589feb69385c6dcc9f
bertrand-d <delphineb57@gmail.com>
2026-02-09 17:55:07 +0100

    add carousel

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
 import BlockHero from "@/components/blocks/BlockHero";
+import CarouselInfinite from "@/components/blocks/CarouselInfinite";
+
 export default function Home() {
   return (
     <main className="mt-[140px]">
       <BlockHero />
+      <section className="max-container">
+        <CarouselInfinite />
+      </section>
     </main>
   );
 }
