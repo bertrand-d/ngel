716b8bc5e3b562b5573e341003bf7b1670385a40
bertrand-d <delphineb57@gmail.com>
2026-02-09 14:06:00 +0100

    add header and font

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="mt-[140px]">
+      <section className="max-container">
+        <div>
+        <h1>Un projet de rénovation durable ?</h1>
+        <h2>Votre spécialiste Ngel proche de chez vous le réalise !</h2>
+        </div>
+        <div>
 
+        </div>
+      </section>
     </main>
   );
 }
