1fd18a54705e4663caa01fb7adac51a786ff619a
bertrand-d <delphineb57@gmail.com>
2026-02-10 14:51:38 +0100

    add image

--- src/components/blocks/BlockHero.jsx
+++ src/components/blocks/BlockHero.jsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import illustration from "../../assets/img/placeholder.png";
-
 export default function BlockHero() {
     return (
         <section className="max-container flex flex gap-md">
-            <div className="min-h-[320px] relative flex flex-col justify-center gap-5 flex-1">
-                <Image src={illustration} alt="illustration" width={735} height={320} className="absolute opacity-50 z-[-1] full h-full object-cover" />
+            <div
+                className="relative min-h-[535px] flex flex-col justify-center gap-5 flex-1 p-10 hero-bg"
+            >
                 <h1 className="h2-style text-secondary-1">Un projet de rénovation durable ?</h1>
                 <h2 className="h1-style text-primary-1">Votre spécialiste Ngel proche de chez vous le réalise !</h2>
             </div>
