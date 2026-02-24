6c93e1a67ded43d1f81ac229a368b4edcf603198
bertrand-d <delphineb57@gmail.com>
2026-02-24 21:55:15 +0100

    fix

--- src/components/blocks/BlockWaranty.jsx
+++ src/components/blocks/BlockWaranty.jsx
@@ -8,9 +8,9 @@ export default function BlockWaranty() {
         <section className="max-container py-sm">
             <h2 className="text-secondary-1 text-center mb-md text-[2.5rem]">Nos garanties !</h2>
             <div className="grid grid-cols-3 gap-2 justify-between items-center w-[80%] mx-auto">
-                <Image src={image1} alt="image" width={180} height={150} />
+                <Image src={image1} alt="image" width={230} height={150} className="mt-[-50px]" />
                 <div className="flex justify-center items-center">
-                <Image src={image2} alt="image" width={150} height={150} />
+                    <Image src={image2} alt="image" width={150} height={150} />
                 </div>
                 <Image src={image3} alt="image" width={150} height={150} className="ml-auto" />
             </div>
