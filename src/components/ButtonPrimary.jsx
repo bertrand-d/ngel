a5189d86b8a2c955357b7ca552d855c917371f15
bertrand-d <delphineb57@gmail.com>
2026-03-02 08:33:45 +0100

    fix anchor

--- src/components/ButtonPrimary.jsx
+++ src/components/ButtonPrimary.jsx
@@ -1,15 +1,17 @@
-import Link from "next/link";
-
-export default function ButtonPrimary({ href = "#form-steps", className }) {
+export default function ButtonPrimary({ className }) {
     return (
-        <Link
-            href={href}
-            className={`block bg-primary-1 py-3 px-5 text-white text-center text-2xl font-bold w-fit hover:bg-secondary-1 transition-colors duration-300 ${className}`}
+        <button
+            onClick={() => {
+                document
+                  .getElementById("form-steps")
+                  ?.scrollIntoView({ behavior: "smooth" })
+              }}
+            className={`cursor-pointer block bg-primary-1 py-3 px-5 text-white text-center text-2xl font-bold w-fit hover:bg-secondary-1 transition-colors duration-300 ${className}`}
         >
             DEMANDER UN DEVIS <br />
             <span className="block font-normal text-sm">
                 Précis - clair - sans surprise
             </span>
-        </Link>
+        </button>
     );
 }
